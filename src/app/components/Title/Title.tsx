2b50ba3414cf427c2dcb9c18574e56258327b0f6
Torben Feldmann <torben.feldmann1@gmail.com>
2021-11-05 11:34:01 +0100

    Add title component

---
+++ src/app/components/Title/Title.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Title.module.css';
+
+function Title(): JSX.Element {
+  return <h1 className={classes.title}>Compare-Companies</h1>;
+}
+
+export default Title;
